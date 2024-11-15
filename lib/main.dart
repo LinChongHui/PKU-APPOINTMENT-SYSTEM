20153119714cc1fa7b8c60264f8a24ddda5f4d1b
Chong Hui <chonghui0205@gmail.com>
2024-11-16 01:57:09 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,24 @@ import 'package:user_profile_management/page/interface.dart';
 import 'const/firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized;
+  WidgetsFlutterBinding.ensureInitialized();
   
   await Firebase.initializeApp(
-    options:DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   
   runApp(const MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:InterfacePage(),
+      home: InterfacePage(),
     );
-}
+  }
 }
