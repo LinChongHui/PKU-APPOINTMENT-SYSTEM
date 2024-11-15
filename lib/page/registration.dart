0510cce82dde9f276cc1faed070a016aebffd9d8
Chong Hui <chonghui0205@gmail.com>
2024-11-16 01:14:39 +0800

    add registration page

---
+++ lib/page/registration.dart
@@ -0,0 +1,234 @@
+// registration.dart
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:user_profile_management/auth_service.dart';
+import 'package:user_profile_management/page/home.dart';
+import 'package:user_profile_management/page/registration.dart';
+import 'package:user_profile_management/page/login.dart';
+
+
+class RegistrationPage extends StatefulWidget {
+  const RegistrationPage({super.key});
+
+  @override
+  State<RegistrationPage> createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _mobileController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _matricNumberController = TextEditingController();
+
+  Future<void> addNewUser(String userId) async {
+    try {
+      await FirebaseFirestore.instance.collection('users').doc(userId).set({
+        'firstName': _firstNameController.text,
+        'lastName': _lastNameController.text,
+        'email': _emailController.text,
+        'mobile': _mobileController.text,
+        'password': _passwordController.text,  // You may want to encrypt the password
+        'matricNumber': _matricNumberController.text,
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('User registered successfully')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error adding user: $e')),
+      );
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Logo
+              Image.asset('assets/PKU.png', height: 80), // Make sure to replace with actual image path
+
+              const SizedBox(height: 16),
+              const Text(
+                'Pusat Kesihatan UTM',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text(
+                'AUTOMANAGER',
+                style: TextStyle(
+                  fontSize: 14,
+                  letterSpacing: 2,
+                ),
+              ),
+              const SizedBox(height: 24),
+
+              // Registration Form
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    _buildTextField(_firstNameController, 'First Name'),
+                    const SizedBox(height: 16),
+                    _buildTextField(_lastNameController, 'Last Name'),
+                    const SizedBox(height: 16),
+                    _buildTextField(_matricNumberController, 'Matric Number'),
+                    const SizedBox(height: 16),
+                    _buildTextField(_emailController, 'Email'),
+                    const SizedBox(height: 16),
+                    _buildTextField(_mobileController, 'Mobile'),
+                    const SizedBox(height: 16),
+                    _buildPasswordField(_passwordController, 'Password'),
+                    const SizedBox(height: 16),
+                    _buildPasswordField(_confirmPasswordController, 'Confirm Password'),
+                    const SizedBox(height: 24),
+
+                    // Sign Up Button
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            try {
+                              // Register the user with Firebase Authentication
+                              UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                                email: _emailController.text,
+                                password: _passwordController.text,
+                              );
+
+                              // If registration is successful, add user data to Firestore
+                              await addNewUser(userCredential.user!.uid);
+
+                              //jump the box with showing sucessful message
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('Registration successful')),
+                              );
+                              
+                              // Navigate to the login page after successful registration
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(builder: (context) => const LoginPage()),
+                              );
+                            } catch (e) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text('Registration failed: $e')),
+                              );
+                            }
+                          }
+                        },
+
+                        /*onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            // Show success message or handle registration
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Registration successful')),
+                            );
+                            // Navigate to login page
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(builder: (context) => const LoginPage()),
+                            );
+                          }
+                        },*/
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          backgroundColor: Colors.teal,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        child: const Text('Sign Up', style: TextStyle(fontSize: 16)),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+
+                    // Sign In Redirection
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("If you already registered "),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => const LoginPage()),
+                            );
+                          },
+                          child: const Text(
+                            "Sign In",
+                            style: TextStyle(color: Colors.teal, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Helper function to build text fields
+  Widget _buildTextField(TextEditingController controller, String labelText) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: labelText,
+        filled: true,
+        fillColor: Colors.grey[200],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide.none,
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) return 'Enter your $labelText';
+        return null;
+      },
+    );
+  }
+
+  // Helper function to build password fields
+  Widget _buildPasswordField(TextEditingController controller, String labelText) {
+    return TextFormField(
+      controller: controller,
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: labelText,
+        filled: true,
+        fillColor: Colors.grey[200],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide.none,
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) return 'Enter your $labelText';
+        if (labelText == 'Confirm Password' && value != _passwordController.text) {
+          return 'Passwords do not match';
+        }
+        return null;
+      },
+    );
+  }
+}
+
+
+
+which line define the  subcollection name? how to I change it to gmail ?
