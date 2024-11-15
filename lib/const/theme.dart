086a406af2f40644c2577fec336c157e7efc7a02
Chong Hui <chonghui0205@gmail.com>
2024-11-16 00:58:32 +0800

    add theme.dart

---
+++ lib/const/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF0D8F83);
+const Color secondaryColor = Colors.black;
+const Color tertiaryColor = Colors.white;
+// Add more colors as needed
