b031182bfa36baf9c4d75daa7f795aefbc2caaa1
Chong Hui <chonghui0205@gmail.com>
2024-11-16 11:07:44 +0800

    add logout page

---
+++ lib/page/logout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LogoutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Logout')),
+      body: const Center(child: Text('Logout Page')),
+    );
+  }
+}
