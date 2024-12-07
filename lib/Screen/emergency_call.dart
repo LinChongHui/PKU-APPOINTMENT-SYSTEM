2a362acf1bc1fbb832555fe88785896341a0ff3d
LIOW CHEE SUAN <147355394+CsL0430@users.noreply.github.com>
2024-12-07 21:44:48 +0800

    Add files via upload

---
+++ lib/Screen/emergency_call.dart
@@ -0,0 +1,132 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart' as url_launcher;
+import 'package:csl_appdev/Widget/appbar_and_backarrow.dart';
+import 'package:csl_appdev/back_end/firebase_readPhoneNum.dart';
+
+class EmergencyCall extends StatefulWidget {
+  const EmergencyCall({super.key});
+
+  @override
+  State<EmergencyCall> createState() => _EmergencyCall();
+}
+
+class _EmergencyCall extends State<EmergencyCall> {
+  bool _showConfirm = false;
+  String _phoneNum = "";
+  final ReadPhoneNum _readPhoneNum = ReadPhoneNum.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPhoneNumber();
+  }
+
+  void _fetchPhoneNumber() async {
+    try {
+      final phoneNumber = await _readPhoneNum.fetchPhoneNumber();
+      setState(() {
+        _phoneNum = phoneNumber;
+      });
+    } catch (error) {
+      _showErrorSnackBar('Failed to fetch phone number');
+    }
+  }
+
+  void _showErrorSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+
+  void executePhoneCall() async {
+    final url = Uri(scheme: 'tel', path: _phoneNum);
+
+    if(await url_launcher.canLaunchUrl(url)) {
+      await url_launcher.launchUrl(url);
+    }
+    else {
+      log("Could not launch phone call");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: AppBarAndBackArrow(title: 'Emergency'),
+      ),
+      
+      floatingActionButton: Stack(
+        children: [
+          if (_showConfirm)
+            Positioned(
+              right: 80,
+              bottom: 16,
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text('Are you sure you want to make a phone call?'),
+                    const SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              _showConfirm = false;
+                            });
+                          },
+                          child: const Text('Cancel'),
+                        ),
+                        const SizedBox(width: 8),
+                        ElevatedButton(
+                          onPressed: () {
+                            executePhoneCall();
+                            setState(() {
+                              _showConfirm = false;
+                            });
+                          },
+                          child: const Text('Call'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              right: 16,
+              bottom: 16,
+              child: FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    _showConfirm = true;
+                  });
+                },
+                child: const Icon(Icons.phone),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
