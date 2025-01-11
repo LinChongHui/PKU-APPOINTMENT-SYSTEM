b29cbac7b4309d799e640977eedbf6d6eb72847d
Chong Hui <147372986+LinChongHui@users.noreply.github.com>
2025-01-11 23:02:57 +0800

    Delete lib directory

--- lib/const/theme.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color primaryColor = Color(0xFF0D8F83); //theme color
-const Color secondaryColor = Colors.black; // words color for white background
-const Color tertiaryColor = Colors.white; // words color for green background
-// Add more colors as needed
